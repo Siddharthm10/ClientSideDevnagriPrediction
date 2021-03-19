38dc65969ebffdea8f7523a4011ae8f520a37803
Siddharthm10 <siddharth.mehtaid@gmail.com>
2021-03-19 10:54:33 +0530

    Initial Commit- Working fine for images

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask_cors import CORS
+from flask import Flask, request, render_template, json, jsonify, send_from_directory
+import json
+import cv2
+import numpy as np
+import io
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", methods=["GET"])
+def main():
+    return render_template('index.html')
+
+
+@app.route("/api/prepare", methods=["POST"])
+def prepare():
+    file = request.files['file']
+    res = preprocessing(file)
+    return json.dumps({"image": res.tolist()})
+
+
+@app.route('/model')
+def model():
+    json_data = json.load(open("./model_dev/model.json"))
+    return jsonify(json_data)
+
+
+@app.route('/<path:path>')
+def load_shards(path):
+    return send_from_directory('model_dev', path)
+
+
+def preprocessing(file):
+    in_memory_file = io.BytesIO()
+    file.save(in_memory_file)
+    data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+    img = cv2.imdecode(data, 0)
+    res = cv2.resize(img, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
+    # file.save("static/UPLOAD/img.png") # saving uploaded img
+    # cv2.imwrite("static/UPLOAD/test.png", res) # saving processed image
+    return res
+
+
+if __name__ == "__main__":
+    app.run()
